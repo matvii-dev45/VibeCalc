dbd7906c1a9308501506702135d4144f7310d5a9
HarinezumiHaven <matvio4545@gmail.com>
2025-11-07 21:18:07 +0100

    GUI Changed

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -10,14 +10,21 @@ pub struct Config {
     pub button_hover_color: String,
     pub button_pressed_color: String,
     pub font_size: u32,
+    pub font_path: Option<String>,
 }
 
+///
+/// Reads the configuration from "config.toml".
+///
 pub fn read_config() -> Result<Config> {
     let config_str = std::fs::read_to_string("config.toml")?;
     let config: Config = toml::from_str(&config_str)?;
     Ok(config)
 }
 
+///
+/// Creates a default configuration.
+///
 pub fn create_default_config() -> Config {
     Config {
         window_width: 400,
@@ -27,9 +34,13 @@ pub fn create_default_config() -> Config {
         button_hover_color: "#528bbd".to_string(),
         button_pressed_color: "#3a6ea5".to_string(),
         font_size: 18,
+        font_path: None,
     }
 }
 
+///
+/// Saves the default configuration to "config.toml".
+///
 pub fn save_default_config() -> anyhow::Result<()> {
     let config = create_default_config();
     let toml_str = toml::to_string_pretty(&config)?;
